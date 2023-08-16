fe1cc3961024f3cc751ddd522b76f21e23c4c392
Santosh Gupta <Santosh.Gupta@harpercollins.com>
2023-08-17 03:18:10 +0530

    Fixed the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,15 +31,15 @@ export default function Header() {
             <div>
                 <ul className="flex space-x-10">
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                    ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
+                    ${pathMatchRoute("/") && "!text-black !border-b-red-500"}`}
                     onClick={()=>navigate("/")}
                     >Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                    ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
+                    ${pathMatchRoute("/offers") && "!text-black !border-b-red-500"}`}
                     onClick={()=>navigate("/offers")}
                     >Offers</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                    ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-red-500"}`}
+                    ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "!text-black !border-b-red-500"}`}
                     onClick={()=>navigate("/profile")}
                     >{pageState}</li>
                 </ul>
